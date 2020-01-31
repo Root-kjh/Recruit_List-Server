62333a6715a3eb739ea6f59c9595aebe3072cd02
Drk <wldnro3141@gmail.com>
2020-02-01 04:01:43 +0900

    MongoDB 연동, 간단한 Service구현(테스트X)

---
+++ src/test/java/com/DrK/persistence/DB_Connect.java
@@ -0,0 +1,25 @@
+package com.DrK.persistence;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+@Log4j
+public class DB_Connect {
+
+	@Setter(onMethod_ = {@Autowired})
+	private MongoTemplate mongoTemplate;
+	
+	@Test
+	public void test_connection() {
+		log.info(mongoTemplate);
+	}
+}
