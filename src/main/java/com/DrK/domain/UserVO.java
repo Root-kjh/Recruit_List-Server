46978ad6e42561d6a73df80cdfe5d969d0e7e173
Drk <wldnro3141@gmail.com>
2020-02-03 17:57:17 +0900

    JPA&Hibernate 연동, Service 부분 에러

--- src/main/java/com/DrK/domain/UserVO.java
+++ src/main/java/com/DrK/domain/UserVO.java
@@ -1,10 +1,26 @@
 package com.DrK.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.Data;
 
 @Data
+@Entity
+@Table(name = "User")
 public class UserVO {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long idx;
+	
+	@Column(name = "id", nullable = false, length = 50)
 	private String id;
+	
+	@Column(name="password", nullable = false, length = 65)
 	private String password;
-	private int idx;
 }
