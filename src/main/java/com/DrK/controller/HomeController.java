8b4e806d61536fc9ccf0bf4eea1aa4796c2e5e37
Drk <wldnro3141@gmail.com>
2020-02-10 17:49:18 +0900

    REST API Security 연동 시도

--- src/main/java/com/DrK/controller/HomeController.java
+++ src/main/java/com/DrK/controller/HomeController.java
@@ -1,39 +1,22 @@
 package com.DrK.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Handles requests for the application home page.
  */
-@Controller
+@RestController
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	@GetMapping("/login")
+	public String login(@RequestParam(value="error",required = false)String error,
+			@RequestParam(value="logout",required = false)String logout) {
+		if(error!=null) 
+			return "error";
+		if(logout!=null)
+			return "logout";
+		return "login";
 	}
-	
 }
