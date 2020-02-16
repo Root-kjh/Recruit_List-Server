3b16fa4fa212d3d1a521a9f3b342117c0aea4820
Drk <wldnro3141@gmail.com>
2020-02-17 03:10:57 +0900

    Cors 추가

--- src/main/java/com/DrK/repositories/UserLikeCompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.DrK.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.DrK.entities.UserLikeCompany;
-
-public interface UserLikeCompanyRepository extends JpaRepository<UserLikeCompany, Integer>{
-
-}
--- src/main/java/com/DrK/repositories/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.DrK.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.DrK.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
