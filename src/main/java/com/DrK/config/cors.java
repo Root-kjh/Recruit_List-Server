addc7cf447a860442ade9fbbe2074cf409e88566
Drk <wldnro3141@gmail.com>
2020-02-18 17:58:34 +0900

    csrf token 추가

--- src/main/java/com/DrK/config/cors.java
+++ src/main/java/com/DrK/config/cors.java
@@ -17,7 +17,7 @@ public class cors implements Filter {
 		response.setHeader("Access-Control-Allow-Origin", "*"); //허용대상 도메인
 		response.setHeader("Access-Control-Allow-Methods", "POST, GET, DELETE, PUT"); 
 		response.setHeader("Access-Control-Max-Age", "3600"); 
-		response.setHeader("Access-Control-Allow-Headers", "x-requested-with, origin, content-type, accept"); 
+		response.setHeader("Access-Control-Allow-Headers", "Origin,Accept,X-Requested-With,Content-Type,Access-Control-Request-Method,Access-Control-Request-Headers,Authorization, x-csrf-token"); 
 		chain.doFilter(req, res); 
     }
     public void init(FilterConfig filterConfig) {}
--- src/main/java/com/DrK/controller/UserContoller.java
+++ src/main/java/com/DrK/controller/UserContoller.java
@@ -1,10 +1,14 @@
 package com.DrK.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,12 +17,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserContoller {
 	
 	@RequestMapping(path = "/login", method = RequestMethod.GET)
-	public String login(@RequestParam(value="error",required = false)String error,
+	public String login(HttpServletRequest request, @RequestParam(value="error",required = false)String error,
 			@RequestParam(value="logout",required = false)String logout) {
 		if(error!=null) 
 			return "error";
 		if(logout!=null)
 			return "logout";
-		return "login";
+		CsrfToken token = (CsrfToken)request.getAttribute(CsrfToken.class.getName());
+		return token.getToken();
+	}
+	
+	@RequestMapping(path="/csrf-token", method = RequestMethod.GET)
+	public @ResponseBody String getCsrf(HttpServletRequest request) {
+		CsrfToken token = (CsrfToken)request.getAttribute(CsrfToken.class.getName());
+		return token.getToken();
 	}
 }
