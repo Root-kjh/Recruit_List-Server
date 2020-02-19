7eb1932dbc54ab59d5099bdad496cd8d80e0c7c9
Drk <wldnro3141@gmail.com>
2020-02-19 17:31:14 +0900

    csrf 토큰 관련 오류 해결

--- src/main/java/com/DrK/controller/UserContoller.java
+++ src/main/java/com/DrK/controller/UserContoller.java
@@ -3,15 +3,15 @@ package com.DrK.controller;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @CrossOrigin("*")
-@RequestMapping("/user")
 public class UserContoller {
 	
 	@RequestMapping(path = "/login", method = RequestMethod.GET)
@@ -21,7 +21,6 @@ public class UserContoller {
 			return "error";
 		if(logout!=null)
 			return "logout";
-		CsrfToken token = (CsrfToken)request.getAttribute(CsrfToken.class.getName());
-		return token.getToken();
+		return "login";
 	}
 }
