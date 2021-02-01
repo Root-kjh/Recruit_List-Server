b275ad0e0be93a0653cdbbe09480d18af08b6029
kjh <wldnro3141@gmail.com>
2021-02-01 17:23:03 +0900

    update service scan base

--- src/main/java/com/DrK/config/root.java
+++ src/main/java/com/DrK/config/root.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan(basePackages = {"com.DrK.Service", "com.DrK.config"})
+@ComponentScan(basePackages = {"com.DrK.service", "com.DrK.config"})
 public class root {
 
 }
