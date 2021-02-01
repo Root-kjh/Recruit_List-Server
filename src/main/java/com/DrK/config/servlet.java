6ee6cd5e92127680b483f60f36ac4a148efd29ea
kjh <wldnro3141@gmail.com>
2021-02-01 17:21:41 +0900

    scan base update

--- src/main/java/com/DrK/config/servlet.java
+++ src/main/java/com/DrK/config/servlet.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-@ComponentScan("com.DrK.Controller")
+@ComponentScan("com.DrK.controller")
 public class servlet implements WebMvcConfigurer{
 
 }
